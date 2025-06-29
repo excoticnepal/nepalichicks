238eaf6b98091d722a7a4665eef9778d38f0cc40
Adarsh Kushwaha <zerotwoadarsh@gmail.com>
2025-06-30 02:59:46 +0530

    done with the commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+// import { visualizer } from 'rollup-plugin-visualizer';
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    // visualizer({ open: true }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Split vendor libraries into separate chunks
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
+
